d409dcb80805771fe596ded1523f4f47d39c8a8a
Hang Shi <hangshiisi@gmail.com>
2019-03-28 19:21:36 -0700

    fix issue in v2

--- federated/hqf/tf_v2_emnist.py
+++ federated/hqf/tf_v2_emnist.py
@@ -6,7 +6,12 @@ from six.moves import range
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras.optimizer_v2 import gradient_descent
+
 from tensorflow_federated import python as tff
+
+#import tensorflow_federated as tff
+
+
 
 nest = tf.contrib.framework.nest
 
@@ -15,6 +20,67 @@ np.random.seed(0)
 tf.compat.v1.enable_v2_behavior()
 
 tff.federated_computation(lambda: 'Hello, World!')()
+
+
+
+
+
+
+emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
+
+
+example_dataset = emnist_train.create_tf_dataset_for_client(
+    emnist_train.client_ids[0])
+
+example_element = iter(example_dataset).next()
+
+example_element['label'].numpy()
+
+
+
+
+
+
+NUM_EPOCHS = 10
+BATCH_SIZE = 20
+SHUFFLE_BUFFER = 500
+
+
+def preprocess(dataset):
+
+  def element_fn(element):
+    return collections.OrderedDict([
+        ('x', tf.reshape(element['pixels'], [-1])),
+        ('y', tf.reshape(element['label'], [1])),
+    ])
+
+  return dataset.repeat(NUM_EPOCHS).map(element_fn).shuffle(
+      SHUFFLE_BUFFER).batch(BATCH_SIZE)
+
+
+def make_federated_data(client_data, client_ids):
+  return [preprocess(client_data.create_tf_dataset_for_client(x))
+          for x in client_ids]
+
+NUM_CLIENTS = 3
+
+sample_clients = emnist_train.client_ids[0:NUM_CLIENTS]
+
+federated_train_data = make_federated_data(emnist_train, sample_clients)
+
+len(federated_train_data), federated_train_data[0]
+
+
+
+preprocessed_example_dataset = preprocess(example_dataset)
+
+sample_batch = nest.map_structure(
+    lambda x: x.numpy(), iter(preprocessed_example_dataset).next())
+
+
+
+
+
 
 
 MnistVariables = collections.namedtuple(
@@ -71,8 +137,8 @@ def get_local_mnist_metrics(variables):
 def aggregate_mnist_metrics_across_clients(metrics):
   return {
       'num_examples': tff.federated_sum(metrics.num_examples),
-      'loss': tff.federated_average(metrics.loss, metrics.num_examples),
-      'accuracy': tff.federated_average(metrics.accuracy, metrics.num_examples)
+      'loss': tff.federated_mean(metrics.loss, metrics.num_examples),
+      'accuracy': tff.federated_mean(metrics.accuracy, metrics.num_examples)
   }
 
 
@@ -102,7 +168,6 @@ class MnistModel(tff.learning.Model):
                                                         tf.float32)),
                                     ('y', tf.TensorSpec([None, 1], tf.int32))])
 
-  # TODO(b/124777499): Remove `autograph=False` when possible.
   @tf.contrib.eager.function(autograph=False)
   def forward_pass(self, batch, training=True):
     del training
@@ -120,7 +185,6 @@ class MnistModel(tff.learning.Model):
 
 class MnistTrainableModel(MnistModel, tff.learning.TrainableModel):
 
-  # TODO(b/124777499): Remove `autograph=False` when possible.
   @tf.contrib.eager.defun(autograph=False)
   def train_on_batch(self, batch):
     output = self.forward_pass(batch)
@@ -141,7 +205,7 @@ state, metrics = iterative_process.next(state, federated_train_data)
 print('round  1, metrics={}'.format(metrics))
 
 
-for round_num in range(2, 11):
+for round_num in range(2, 101):
   state, metrics = iterative_process.next(state, federated_train_data)
   print('round {:2d}, metrics={}'.format(round_num, metrics))
 
